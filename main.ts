f7eac6f52c7f0723e5fa7ec6ca439e9a36d60fb1
DeputyRyan <deputyryan32@gmail.com>
2026-01-18 17:56:26 -0500

    First working BETA COPY.

--- main.ts
+++ main.ts
@@ -1,5 +1,35 @@
+// Sprites Setup/Utils
 namespace SpriteKind {
-
 }
-let myCrackHead: Sprite = null
-scene.setBackgroundImage(assets.image`myBackground`)
+let myCrackHead = sprites.create(assets.image`myPlayerImage`, SpriteKind.Player);
+scene.cameraFollowSprite(myCrackHead)
+myCrackHead.ay = 500
+
+// Info Setup
+info.setScore(0)
+info.setLife(1)
+// Main Scene Setup
+scene.setBackgroundColor(6)
+tiles.setCurrentTilemap(tilemap`level`)
+tiles.placeOnTile(myCrackHead, tiles.getTileLocation(0, 18))
+controller.moveSprite(myCrackHead, 85, 0)
+
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (myCrackHead.isHittingTile(CollisionDirection.Bottom)) {
+        myCrackHead.vy = -250
+    }
+})
+
+// Death Function
+info.onLifeZero(function(){
+    info.setScore(0)
+    scene.setBackgroundColor(6)
+    tiles.setCurrentTilemap(tilemap`level`)
+    tiles.placeOnTile(myCrackHead, tiles.getTileLocation(0, 18))
+    controller.moveSprite(myCrackHead, 85, 0)
+    info.setLife(1)
+})
+
+scene.onOverlapTile(SpriteKind.Player, assets.tile`deathTile`, function(){
+    info.changeLifeBy(-1)
+})
